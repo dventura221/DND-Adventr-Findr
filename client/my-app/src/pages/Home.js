d68db5d40130c9095c623d5e49c6dce434b0d62d
Dan Ventura <danice.ventura@gmail.com>
2022-03-29 12:21:01 -0600

    deleted fluff files from react, started home page and added front end folders

---
+++ client/my-app/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Header 1</h1>
+    </div>
+  )
+}
+
+export default Home
